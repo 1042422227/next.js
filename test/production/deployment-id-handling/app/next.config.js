c275ea46830358372bca49cd902a98f178b96489
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-15 21:28:23 +0100

    add test

--- test/production/deployment-id-handling/app/next.config.js
+++ test/production/deployment-id-handling/app/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  deploymentId: process.env.CUSTOM_DEPLOYMENT_ID,
+  deploymentId:
+    process.env.IMMUTABLE_DEPLOYMENT_ID ?? process.env.CUSTOM_DEPLOYMENT_ID,
   experimental: {
     useSkewCookie: Boolean(process.env.COOKIE_SKEW),
+    immutableDeploymentId: process.env.IMMUTABLE_DEPLOYMENT_ID
+      ? `imm-${process.env.IMMUTABLE_DEPLOYMENT_ID}`
+      : undefined,
   },
 }
